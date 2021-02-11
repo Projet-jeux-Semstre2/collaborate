d2e5af10ad5a46a43657d97200001cbf2235eeda
Lampape <62471565+Lampape@users.noreply.github.com>
2021-02-11 20:41:53 +0100

    Pti LDpepouz

--- teste/Assets/Script/Entities_Manager.cs
+++ teste/Assets/Script/Entities_Manager.cs
@@ -6,23 +6,42 @@ using Random = UnityEngine.Random;
 
 public class Entities_Manager : MonoBehaviour
 {
+    [Header("Attraction")]
     public float pullRadius = 2;
     public float pullForce;
 
     public float pullForceMin;
     public float pullForceMax;
-    
-
+    [Space(10)]
+    [Tooltip("Materiaux changeant")]
     public Material[] ChangeMaterials;
     private Rigidbody _rb;
 
+    [Header("Rapport avec la formation d'un core")]
     public bool hasCore;
     public bool isInGroups;
+    [Tooltip("Le core auquel j'appartiens")]
     public GameObject core;
     private GameObject _instCore;
     public GameObject myCore;
-    public List<GameObject> inMyGroup;
+    [Tooltip("Nb minimum de particule pour former un core (sans compter elle même(si 5 alors faut 6)")]
     public int minCoreParticle = 2;
+    [Space(2)]
+    public List<GameObject> inMyGroup;
+    
+    
+    
+    
+    public enum Forme {Cube, Cone, Triangle, Icosphere}
+    [Header("Choix de la forme")]
+    [Tooltip("Choisis la forme que tu veut pour cette entité")]
+    public Forme forme;
+    public Mesh cubeMesh;
+    public Mesh coneMesh;
+    public Mesh triangleMesh;
+    public Mesh icosphereMesh;
+    
+    private Mesh _myMesh;
     private void Start()
     {
         pullRadius = Random.Range(1, 7);
@@ -33,9 +52,15 @@ public class Entities_Manager : MonoBehaviour
 
     public void FixedUpdate()
     {
+        //Changement forme
+        ChoosingForme();
+        
+        
+        
+        
         if (!hasCore) //attirance des entités vers sois meme si je ne fais pas parti d'un groupe
         {
-            GetComponent<SphereCollider>().enabled = true;
+            GetComponentInChildren<SphereCollider>().enabled = true;
             
             foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius) )
             {
@@ -49,6 +74,10 @@ public class Entities_Manager : MonoBehaviour
                     collider.GetComponent<Rigidbody>().AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
                 }
             }
+        }
+        else
+        {
+            GetComponentInChildren<SphereCollider>().enabled = false;
         }
         
 
@@ -105,13 +134,6 @@ public class Entities_Manager : MonoBehaviour
             {
                 inMyGroup.Add(other.gameObject);
             }
-            //other.GetComponent<Entities_Manager>().isInGroups = true;
-
-            /*if (!other.GetComponent<Entities_Manager>().hasCore && other.GetComponent<Entities_Manager>().isInGroups && isInGroups)
-            {
-                _instCore = Instantiate(core, transform.position, Quaternion.identity);
-                print("je créer un core");
-            }*/
         }
     }
 
@@ -123,4 +145,34 @@ public class Entities_Manager : MonoBehaviour
             
         }
     }
+
+    void ChoosingForme()
+    {
+        if (forme == Forme.Cube)
+        {
+            _myMesh = cubeMesh;
+            gameObject.name = "Cube";
+        }
+        if (forme == Forme.Cone)
+        {
+            _myMesh = coneMesh;
+            gameObject.name = "Cone";
+        }
+        if (forme == Forme.Triangle)
+        {
+            _myMesh = triangleMesh;
+            gameObject.name = "Triangle";
+        }
+        if (forme == Forme.Icosphere)
+        {
+            _myMesh = icosphereMesh;
+            gameObject.name = "Icosphere";
+        }
+        
+        GetComponent<MeshFilter>().mesh = _myMesh;
+        GetComponent<MeshCollider>().sharedMesh = _myMesh;
+        
+    }
+
+    
 }
