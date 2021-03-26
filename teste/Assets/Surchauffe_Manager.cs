07ff8a0264557a87990b2543c9fc1063532a7fa5
Lampape <62471565+Lampape@users.noreply.github.com>
2021-03-26 18:29:35 +0100

    surchauffe

--- teste/Assets/Surchauffe_Manager.cs
+++ teste/Assets/Surchauffe_Manager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Surchauffe_Manager : MonoBehaviour
 {
@@ -9,15 +10,31 @@ public class Surchauffe_Manager : MonoBehaviour
     public float niveauSurchauffe;
 
     public float t_forLooseSurchauffe;
-    
+
+    public RectTransform surchauffeImage;
+
     private float t;
+
+    
+
     private void Update()
     {
+        float add = 1115 / surchauffe;
+        if (niveauSurchauffe > 0)
+        {
+            surchauffeImage.sizeDelta = Vector2.Lerp(surchauffeImage.sizeDelta, new Vector2(add * niveauSurchauffe, surchauffeImage.sizeDelta.y), Time.time);
+        }
+        
+        
         t += Time.deltaTime;
         if (niveauSurchauffe >= 1 && t >= t_forLooseSurchauffe)
         {
             niveauSurchauffe--;
             t = 0;
         }
+        
+        
+        
+        
     }
 }
